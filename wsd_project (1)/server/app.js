fbb96698308d767825da5751203d936993677168
lukasblomquist <140920639+lukasblomquist@users.noreply.github.com>
2025-05-02 16:52:37 +0300

    Update app.js

--- wsd_project (1)/server/app.js
+++ wsd_project (1)/server/app.js
@@ -1,5 +1,5 @@
 
-import { Hono } from "https://deno.land/x/hono@v3.2.6/mod.js";
+import { Hono } from "https://deno.land/x/hono@v3.2.6/mod.ts";:contentReference[oaicite:19]{index=19}
 
 const app = new Hono();
 
